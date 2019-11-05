93fc1c7022183660ad4386cf8f3be9d2adfd0360
Leonid Petrov <frostwolm@gmail.com>
2019-11-05 16:21:15 +0300

    feat: base structure

---
+++ app.js
@@ -0,0 +1,8 @@
+const CONTAINER_ID = 'test';
+const FORM_HTML = ``;
+
+function initform(evt) {
+    const containerElement = document.getElementById(CONTAINER_ID);
+}
+
+document.addEventListener('DOMContentLoaded', initform);
