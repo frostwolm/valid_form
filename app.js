5a2096d4df7e49dfaedcb3b41149eb9fb0844503
Leonid Petrov <frostwolm@gmail.com>
2019-11-06 20:51:22 +0300

    BIG FAT COMMIT

--- app.js
+++ app.js
@@ -1,86 +1,108 @@
-const CONTAINER_ID = 'test';
-const FORM_HTML = `<form action="" method="post" id="form-opros">
-        <div name="name-container" class="name-container">
-            <input type="text" name="" id="">
-            <div class="radio-container"><input type="radio" name="sex-radio-1" id="" value="m"><input type="radio" name="sex-radio-1" id="" value="f"></div>
-        </div>
-        <div name="name-container" class="name-container">
-            <input type="text" name="" id="">
-            <div class="radio-container"><input type="radio" name="sex-radio-2" id="" value="m"><input type="radio" name="sex-radio-2" id="" value="f"></div>
-        </div>
-        <div name="name-container" class="name-container">
-            <input type="text" name="" id="">
-            <div class="radio-container"><input type="radio" name="sex-radio-3" id="" value="m"><input type="radio" name="sex-radio-3" id="" value="f"></div>
-        </div>
-        <div name="name-container" class="name-container">
-            <input type="text" name="" id="">
-            <div class="radio-container"><input type="radio" name="sex-radio-4" id="" value="m"><input type="radio" name="sex-radio-4" id="" value="f"></div>
-        </div>
-        <div name="name-container" class="name-container">
-            <input type="text" name="" id="">
-            <div class="radio-container"><input type="radio" name="sex-radio-5" id="" value="m"><input type="radio" name="sex-radio-5" id="" value="f"></div>
-        </div>
-        <div name="name-container" class="name-container">
-            <input type="text" name="" id="">
-            <div class="radio-container"><input type="radio" name="sex-radio-6" id="" value="m"><input type="radio" name="sex-radio-6" id="" value="f"></div>
-        </div>
-        <div name="name-container" class="name-container">
-            <input type="text" name="" id="">
-            <div class="radio-container"><input type="radio" name="sex-radio-7" id="" value="m"><input type="radio" name="sex-radio-7" id="" value="f"></div>
-        </div>
-        <div name="name-container" class="name-container">
-            <input type="text" name="" id="">
-            <div class="radio-container"><input type="radio" name="sex-radio-8" id="" value="m"><input type="radio" name="sex-radio-8" id="" value="f"></div>
-        </div>
-        <div name="name-container" class="name-container">
-            <input type="text" name="" id="">
-            <div class="radio-container"><input type="radio" name="sex-radio-9" id="" value="m"><input type="radio" name="sex-radio-9" id="" value="f"></div>
-        </div>
-        <div name="name-container" class="name-container">
-            <input type="text" name="" id="">
-            <div class="radio-container"><input type="radio" name="sex-radio-10" id="" value="m"><input type="radio" name="sex-radio-10" id="" value="f"></div>
-        </div>
-        <button type="submit">Submit!</button>
-    </form>`;
-    const NOT_VALID_CSS_CLASS = 'form__input--notvalid';
 
-function initform(evt) {
-    const containerElement = document.getElementById(CONTAINER_ID);
-    containerElement.insertAdjacentHTML('afterbegin', FORM_HTML);
-    const formElement = containerElement.firstChild;
-    formElement.addEventListener('submit', onSubmitForm);
-}
 
-function onSubmitForm(evt) {
-    evt.preventDefault();
-    let isPrepared = true;
-    const lstFields = evt.currentTarget.getElementsByClassName('name-container');
-    for (let j = 0; j < lstFields.length; j++) {
-        const field = lstFields[j];
-        let lstInputs = field.getElementsByTagName('input');
-        for (let i = 0; i < lstInputs.length; i++) {
-            const input = lstInputs[i];
-            let isValid = true;
-            if (input.getAttribute('type') === 'radio') {
-                isValid = input.checked;
-            }
-            if (!isValid) {
-                if (!field.classList.contains(NOT_VALID_CSS_CLASS)) {
-                    field.classList.add(NOT_VALID_CSS_CLASS);
-                }
-            }else{
-                if (field.classList.contains(NOT_VALID_CSS_CLASS)) {
-                    field.classList.remove(NOT_VALID_CSS_CLASS);
-                    break;
-                }  
-            }
-            isPrepared = isPrepared && isValid;
+class FormValid{
+    options = {};
+    CONTAINER_ID = '';
+
+    constructor(containerId, options){
+        if (containerId === undefined) {
+            console.error(`Form valid: set container id`);
         }
+        
+        this.setDefaultOptions(options);
+
+        this.CONTAINER_ID = containerId;
+        document.addEventListener('DOMContentLoaded', this.initform.bind(this));
     }
 
-    if (isPrepared) {
-        evt.currentTarget.submit();
-    }  
+    setDefaultOptions(options){
+        options = options || {};
+        this.options.NOT_VALID_CSS_CLASS = options.NOT_VALID_CSS_CLASS || 'form__input--notvalid';
+        this.options.FORM_HTML = options.FORM_HTML || `<form action="" method="post" id="form-opros">
+                                <div name="name-container" class="name-container">
+                                    <input type="text" name="" id="">
+                                    <div class="radio-container"><input type="radio" name="sex-radio-1" id="" value="m"><input type="radio" name="sex-radio-1" id="" value="f"></div>
+                                </div>
+                                <div name="name-container" class="name-container">
+                                    <input type="text" name="" id="">
+                                    <div class="radio-container"><input type="radio" name="sex-radio-2" id="" value="m"><input type="radio" name="sex-radio-2" id="" value="f"></div>
+                                </div>
+                                <div name="name-container" class="name-container">
+                                    <input type="text" name="" id="">
+                                    <div class="radio-container"><input type="radio" name="sex-radio-3" id="" value="m"><input type="radio" name="sex-radio-3" id="" value="f"></div>
+                                </div>
+                                <div name="name-container" class="name-container">
+                                    <input type="text" name="" id="">
+                                    <div class="radio-container"><input type="radio" name="sex-radio-4" id="" value="m"><input type="radio" name="sex-radio-4" id="" value="f"></div>
+                                </div>
+                                <div name="name-container" class="name-container">
+                                    <input type="text" name="" id="">
+                                    <div class="radio-container"><input type="radio" name="sex-radio-5" id="" value="m"><input type="radio" name="sex-radio-5" id="" value="f"></div>
+                                </div>
+                                <div name="name-container" class="name-container">
+                                    <input type="text" name="" id="">
+                                    <div class="radio-container"><input type="radio" name="sex-radio-6" id="" value="m"><input type="radio" name="sex-radio-6" id="" value="f"></div>
+                                </div>
+                                <div name="name-container" class="name-container">
+                                    <input type="text" name="" id="">
+                                    <div class="radio-container"><input type="radio" name="sex-radio-7" id="" value="m"><input type="radio" name="sex-radio-7" id="" value="f"></div>
+                                </div>
+                                <div name="name-container" class="name-container">
+                                    <input type="text" name="" id="">
+                                    <div class="radio-container"><input type="radio" name="sex-radio-8" id="" value="m"><input type="radio" name="sex-radio-8" id="" value="f"></div>
+                                </div>
+                                <div name="name-container" class="name-container">
+                                    <input type="text" name="" id="">
+                                    <div class="radio-container"><input type="radio" name="sex-radio-9" id="" value="m"><input type="radio" name="sex-radio-9" id="" value="f"></div>
+                                </div>
+                                <div name="name-container" class="name-container">
+                                    <input type="text" name="" id="">
+                                    <div class="radio-container"><input type="radio" name="sex-radio-10" id="" value="m"><input type="radio" name="sex-radio-10" id="" value="f"></div>
+                                </div>
+                                <button type="submit">Submit!</button>
+                            </form>`;
+    }
+
+    initform(evt) {
+        const containerElement = document.getElementById(this.CONTAINER_ID);
+        containerElement.insertAdjacentHTML('afterbegin', this.options.FORM_HTML);
+        const formElement = containerElement.firstChild;
+        formElement.addEventListener('submit', this.onSubmitForm.bind(this));
+    }
+
+    onSubmitForm(evt) {
+        evt.preventDefault();
+        let isFormValid = true;
+        const lstFields = evt.currentTarget.getElementsByClassName('name-container');
+        for (let j = 0; j < lstFields.length; j++) {
+            const field = lstFields[j];
+            let lstInputs = field.getElementsByTagName('input');
+            let isInputValid = false;
+            for (let i = 0; i < lstInputs.length; i++) {
+                const input = lstInputs[i];
+                
+                if (input.getAttribute('type') === 'radio') {
+                    isInputValid = isInputValid || input.checked;
+                }
+            }
+            
+            isFormValid = isFormValid && isInputValid;
+            
+            if (isInputValid) {
+                if (field.classList.contains(this.options.NOT_VALID_CSS_CLASS)) {
+                    field.classList.remove(this.options.NOT_VALID_CSS_CLASS);
+                }
+            }else{
+                if (!field.classList.contains(this.options.NOT_VALID_CSS_CLASS)) {
+                    field.classList.add(this.options.NOT_VALID_CSS_CLASS);
+                }  
+            }
+        }
+
+        if (isFormValid) {
+            evt.currentTarget.submit();
+        }  
+    }
 }
 
-document.addEventListener('DOMContentLoaded', initform);
+const formValid = new FormValid('test');
